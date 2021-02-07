54da6d0f361c1a63269beaac120707136b2920ac
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-02-07 18:00:08 +0600

    display details working

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const clickOnSearch = id => {
-    const searchInput = document.getElementById(id);    
+// const inputId = document.getElementById('search-input');
+const clickOnSearch = inputId => {
+    const searchInput = document.getElementById(inputId);    
 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput.value}`)
         .then(res => res.json())
@@ -10,16 +11,34 @@ const clickOnSearch = id => {
                       
             foods.forEach(food => {
                 const foodItemDiv = document.createElement('div');
-                foodItemDiv.className = 'food-item';               
+                foodItemDiv.className = 'food-item';                             
+                // foodItemDiv.onclick = displayFoodDetails('testing Div');                             
 
                 const foodInfo = `
+                <div onclick = "displayFoodDetails('${food.strMeal}','${food.strMealThumb}')">
                 <img class="width-resize" src="${food.strMealThumb}">
                 <h3>${food.strMeal}</h3>
+                </div>
                 `;
                 foodItemDiv.innerHTML = foodInfo;
-                displayFoodItemsDiv.appendChild(foodItemDiv);
+                displayFoodItemsDiv.appendChild(foodItemDiv);              
             });            
-        }    
+        };   
+};
+
+const displayFoodDetails = (name,foodImage) => {
+    console.log(name);
+    // const foodName = name.innerHTML; ...........remain comment ................
+    const displayFoodName = document.getElementById("display-details");     
+
+    const ingredientsSection = `
+    <img id="width-resize-id" src="${foodImage}">
+    <h1>${name}</h1>
+    <p>${name}</p>
+    `;
+    displayFoodName.innerHTML = ingredientsSection;    
+
+    console.log(ingredientsSection);
 };
 
 
